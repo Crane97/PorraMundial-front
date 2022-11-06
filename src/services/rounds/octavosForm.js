3ad7e96eab337285821aaf9760fa360090c6ea4e
Jorge Ruiz <jorge@macdiego.com>
2022-11-06 17:13:30 +0100

    commitear

---
+++ src/services/rounds/octavosForm.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Select from "./select";
+
+function OctavosComponent(props) {
+  return (
+    <div>
+      <div>
+        {" "}
+        <h4>{"Octavos de final "}</h4>
+      </div>
+      <Select id={"Octavos ganador 1"} label={"Octavos ganador 1"} teams={props.teams}/>
+      <Select id={"Octavos ganador 2"} label={"Octavos ganador 2"} teams={props.teams}/>
+      <Select id={"Octavos ganador 3"} label={"Octavos ganador 3"} teams={props.teams}/>
+      <Select id={"Octavos ganador 4"} label={"Octavos ganador 4"} teams={props.teams}/>
+      <Select id={"Octavos ganador 5"} label={"Octavos ganador 5"} teams={props.teams}/>
+      <Select id={"Octavos ganador 6"} label={"Octavos ganador 6"} teams={props.teams}/>
+      <Select id={"Octavos ganador 7"} label={"Octavos ganador 7"} teams={props.teams}/>
+      <Select id={"Octavos ganador 8"} label={"Octavos ganador 8"} teams={props.teams}/>
+    </div>
+  );
+}
+
+export default OctavosComponent;
