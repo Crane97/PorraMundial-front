30dc0b41f51006a2328397e3ddd796f0e18a7b42
Jorge Ruiz <jorge@macdiego.com>
2022-11-08 01:08:35 +0100

    Simply window navigation

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,11 +2,20 @@ import React from "react";
 import '../styles/navbar.css';
 
 function Navbar() {
+
+  function handleAddUser(e){
+    window.location = "/nuevoJugador";
+  }
+
+  function handlePorraMundial(e){
+    window.location = "/";
+  }
+
   return (
     <div>
       <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand marginLeft">Porra mundial 2022</a>
-          <button class="btn btn-success mx-4" type="submit">
+        <a class="navbar-brand marginLeft" onClick={handlePorraMundial}>Porra mundial 2022</a>
+          <button class="btn btn-success mx-4" type="submit" onClick={handleAddUser}>
             Crear apuesta
           </button>
       </nav>
