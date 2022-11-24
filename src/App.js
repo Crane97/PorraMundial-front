ccee6fc001c82c4317c40d595ea8948013385fea
Jorge Ruiz <jorge@macdiego.com>
2022-11-24 10:16:15 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import AddPlayer from './pages/addPlayer';
 import Navbar from './pages/navbar';
-import Bets from "./services/playerinfo/groupsBets";
 import PlayerBet from "./pages/playerBet";
 
 
